c8210d2c27ccf65a298624c6b8337293eda6e8fa
Leonardo <leonardomartinslopes2@gmail.com>
2025-04-02 22:32:55 -0300

    Adição de ideia nova de código

--- Trabalho-1.asm
+++ Trabalho-1.asm
@@ -3,11 +3,27 @@
     A:  .word 0,2,4,6,8,10,12,14,16,18
     B:  .word 1,3,5,7,9,11,13,15,17,19
     R:  .word 0,0,0,0,0,0,0,0,0,0
+    T:  .word 10
 
         .globl main
     main:
-        #ver se posso fazer sub x0 x0 para  limpar reg
-        addi x0, 0
+
+        sub r0, r0 ; r0 = 0
+        not r0, r0 ; r0 = 1
+        ld r3, T ; r3 = 10 ULTIMO REG
+    loop:
+        ld r1, A ; r1 = A[i]
+        ld r2, B ; r2 = b[i]
+        add r1, r2 ; r1 + r2
+        st R, r1 ; t[i] = guarda a soma
+        add R, r0 ; endereco T + 1
+        add A, r0 ; deslocamento do vetor 
+        add B, r0 
+        sub r3, r0; decrementa contador
+        brzr r3, fora ; se r3 == 0 sai
+        ji loop ; NAO SEI SE FUNCIONA
+
+
         addi t7, 4
         addi
         ld t0, A; t0 = endereço A
