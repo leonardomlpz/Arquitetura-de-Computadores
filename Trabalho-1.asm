dec54ac80d0ab18c81fa612d5ec11230be06ab9a
Leonardo <leonardomartinslopes2@gmail.com>
2025-04-04 11:15:07 -0300

    [change] [remove] remocao e mudanca no codigo, adicionar vetores em codigo

--- Trabalho-1.asm
+++ Trabalho-1.asm
@@ -1,16 +1,9 @@
-        .text
-        .data
-    A:  .word 0,2,4,6,8,10,12,14,16,18
-    B:  .word 1,3,5,7,9,11,13,15,17,19
-    R:  .word 0,0,0,0,0,0,0,0,0,0
-    T:  .word 10
-
-        .globl main
     main:
 
-        sub r0, r0 ; r0 = 0
-        not r0, r0 ; r0 = 1
-        ld r3, T ; r3 = 10 ULTIMO REG
+        sub r1, r1 ; r1 = 0
+        not r1, r1 ; r1 = 1
+        addi 7 ; r0 == 7
+        addi 3 ; r0 == 7+3=10
     loop:
         ld r1, A ; r1 = A[i]
         ld r2, B ; r2 = b[i]
@@ -23,21 +16,4 @@
         brzr r3, fora ; se r3 == 0 sai
         ji loop ; NAO SEI SE FUNCIONA
 
-
-        addi t7, 4
-        addi
-        ld t0, A; t0 = endereço A
-        ld t1, B
-        ld t2, R
-        addi t3, 10; t4 fim do laco
-
-        ld t4, t0; t4 = A[i]
-        ld t5, t1; t5 = B[i]
-        ld t6, t2; t6 = R[i]
-
-loop:   add t6, t4; 0 + A[i]
-        add t6, t5; A[i] + B[i]
-        st t2, t6;; armazena soma em R
-        add t0, t7;; soma 4 no endereco dos vetores
-        add t1, t7
-        add t2, t7
+        ebreak
